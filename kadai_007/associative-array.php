728a1b3cba025fae76705c815267c5b6b7d50acd
juzadeNube <122423453+HideyukiYokota@users.noreply.github.com>
2023-06-11 21:16:50 +0900

    Update associative-array.php

--- kadai_007/associative-array.php
+++ kadai_007/associative-array.php
@@ -1,8 +1,8 @@
 <html>
 <?php
  $array = ['name' => 'onion','price' => 200,'weight' => 160];
- echo $array['name'];
- echo '<Br>';
+//  echo $array['name'];
+//  echo '<Br>';
  print_r($array);
 ?>
 </html>
