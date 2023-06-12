fd40d6b695a4ea1d7edf5f183488a1c9bb32fe38
juzadeNube <122423453+HideyukiYokota@users.noreply.github.com>
2023-06-12 15:47:42 +0900

    Update confirmation.php

--- kadai_021/confirmation.php
+++ kadai_021/confirmation.php
@@ -8,6 +8,6 @@
         <h2>入力内容をご確認ください。</h2>
         <p>お名前:<?php echo $_POST['user_name'];?></p>
         <p>性別:<?php echo $_POST['gender'];?></p>
-        <p>お問い合わせ種別:<?php echo $_POST['address'];?></p>
+        <p>メールアドレス:<?php echo $_POST['address'];?></p>
     </body>
 </html>
